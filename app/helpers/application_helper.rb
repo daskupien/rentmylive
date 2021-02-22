1514704dd9c3e8458b9050f1d386510daa17aea7
Jonas Kozik <jonas.kozik@gmail.com>
2021-02-22 18:01:26 +0100

    default is now logged in, log out is still missing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
-    user_signed_in?
-    current_user
+
+    def user_signed_in?
+        false || true
+    end
+    
+    def current_user
+        @current_user ||= User.find_by(id: session[:user])
+    end
+ 
 end
