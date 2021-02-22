af6c9d9b9b8dec11399d80d00b48db859a75b6d6
Begana Choi <begana001.jpg@gmail.com>
2021-02-22 18:23:23 +0100

    merged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-=======
-    
-    before_action :authenticate_user!
->>>>>>> master
-
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
