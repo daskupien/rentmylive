94db1de89d4224f4f596a8781051203085c43ce0
Daniel Skupien <info@danielskupien.de>
2021-02-23 12:45:17 +0100

    added seeds (3 user, 3 lives)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(email: 'euan@email.com', encrypted_password: '1234', first_name: 'Euan', last_name: 'Last', address: 'street', gender: 'male', host_bio:'crazy guy from around')
+User.destroy_all
+Life.destroy_all
 
-# User.create(email: 'claire@email.com', encrypted_password: '1234', first_name: 'Claire', last_name: 'Last', address: 'street', gender: 'female', host_bio:'crazy girls from france')
+euan = User.create!(email: 'euan@email.com', password: '1234567', first_name: 'Euan', last_name: 'Last', address: 'street', gender: 'male', host_bio:'crazy guy from around')
 
-# User.create(email: 'jim@email.com', encrypted_password: '1234', first_name: 'Jim', last_name: 'Last', address: 'street', gender: 'male', host_bio:'Jimmy trimmi')
+claire = User.create!(email: 'claire@email.com', password: '1234567', first_name: 'Claire', last_name: 'Last', address: 'street', gender: 'female', host_bio:'crazy girls from france')
 
-# TODO Use the ones above after migration works and delete the three down here
+jim = User.create!(email: 'jim@email.com', password: '1234567', first_name: 'Jim', last_name: 'Last', address: 'street', gender: 'male', host_bio:'Jimmy trimmi')
 
-User.create(email: 'euan@email.com', encrypted_password: '1234')
-User.create(email: 'claire@email.com', encrypted_password: '1234')
-User.create(email: 'jim@email.com', encrypted_password: '1234')
+Life.create!(description: 'This is a description', img_url: 'http://images4.fanpop.com/image/photos/23700000/Funny-random-23797915-1000-981.jpg', price_per_day: 100, city: 'New York', job: 'stripper', family: 'all weird people', relationship: 'paired', hobby: 'dancing', user_id: euan.id)
 
+Life.create!(description: 'This is a description', img_url: 'https://media.nesta.org.uk/images/Predictions-2019_Twitter_02.width-1200.png', price_per_day: 200, city: 'Berlin', job: 'gardener', family: 'all weird people', relationship: 'paired', hobby: 'dancing', user_id: claire.id)
 
-Life.create(description: 'This is a description', image: 'http://images4.fanpop.com/image/photos/23700000/Funny-random-23797915-1000-981.jpg', price_per_day: 100, city: 'New York', job: 'stripper', family: 'all weird people', relationship: 'paired', hobby: 'dancing', user_id: 1)
-
-Life.create(description: 'This is a description', image: 'https://media.nesta.org.uk/images/Predictions-2019_Twitter_02.width-1200.png', price_per_day: 200, city: 'Berlin', job: 'gardener', family: 'all weird people', relationship: 'paired', hobby: 'dancing', user_id: 2)
-
-Life.create(description: 'This is a description', image: 'http://images2.fanpop.com/image/photos/14300000/Beautiful-Wallpaper-random-14312356-2560-1600.jpg', price_per_day: 150, city: 'Munich', job: 'shark saver', family: 'too many', relationship: 'paired', hobby: 'dancing', user_id: 3)
+Life.create!(description: 'This is a description', img_url: 'http://images2.fanpop.com/image/photos/14300000/Beautiful-Wallpaper-random-14312356-2560-1600.jpg', price_per_day: 150, city: 'Munich', job: 'shark saver', family: 'too many', relationship: 'paired', hobby: 'dancing', user_id: jim.id)
