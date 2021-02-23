9c37dabe1e9cd92e1bbd4f6b95033d7c5f12e7e7
Jonas Kozik <jonas.kozik@gmail.com>
2021-02-23 11:12:03 +0100

    devise finally setup - might change some stuff on the login/signup meta info later but for now it's working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
 
-    def user_signed_in?
-        false || true
-    end
-    
-    def current_user
-        @current_user ||= User.find_by(id: session[:user])
-    end
+ 
+
  
 end
