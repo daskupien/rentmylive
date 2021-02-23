db662d5441fcddd8c6b40534de64a8c05b59bd31
Daniel Skupien <info@danielskupien.de>
2021-02-23 21:09:12 +0100

    added profile view and link to profile in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lives, only: [ :index, :show, :new, :create]
-
+  resources :users, only: [ :show ]
 end
