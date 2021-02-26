604e957a17052efea3ef716cf10cfffbb9ea8bf0
Daniel Skupien <info@danielskupien.de>
2021-02-26 14:49:27 +0100

    rejact and cancel is working

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   resources :lives, only: [ :index, :show, :new, :create, :destroy] do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :users, only: [ :show ] do
+  resources :users, only: [ :show ]
+  resources :bookings, only: [ :destroy ]
     # resources :bookings, only: [ :confirm_booking, :reject_booking ]
-    patch '/confirm_booking/:id', to: 'bookings#confirm_booking', as: :confirm
-    patch '/reject_booking/:id', to: 'bookings#reject_booking', as: :reject
-  end
+  patch '/confirm_booking/:id', to: 'bookings#confirm_booking', as: :confirm
+  patch '/reject_booking/:id', to: 'bookings#reject_booking', as: :reject
 end
